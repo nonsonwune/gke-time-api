415793468274be78d7cbde7569bf3950c8a6a003
Nonso Nwune <nonsonwune@Nonsos-MacBook-Pro.local>
2024-08-31 23:00:51 +0100

    mmmm

--- terraform/kubernetes_provider.tf
+++
@@ -1,7 +0,0 @@
-provider "kubernetes" {
-  host  = "https://${google_container_cluster.time_api_cluster.endpoint}"
-  token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.time_api_cluster.master_auth[0].cluster_ca_certificate
-  )
-}
