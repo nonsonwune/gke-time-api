aeea05543123ca3487210bd3e2e13476adccf878
Nonso Nwune <nonsonwune@Nonsos-MacBook-Pro.local>
2024-09-01 12:37:21 +0100

    Update outputs.tf

--- terraform/modules/gke/outputs.tf
+++ terraform/modules/gke/outputs.tf
@@ -10,6 +10,7 @@ output "cluster_endpoint" {
   description = "GKE Cluster Host"
 }
 
+
 output "cluster_ca_certificate" {
   value       = data.google_container_cluster.time_api_cluster.master_auth[0].cluster_ca_certificate
   description = "GKE Cluster CA Certificate"
