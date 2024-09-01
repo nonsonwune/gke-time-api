2998f2dbb3e1ac5d6fcaa1b5eb184fb4f3f65f2a
Nonso Nwune <nonsonwune@Nonsos-MacBook-Pro.local>
2024-09-01 12:24:06 +0100

    Update outputs.tf

--- terraform/modules/gke/outputs.tf
+++ terraform/modules/gke/outputs.tf
@@ -1,15 +1,17 @@
+# modules/gke/outputs.tf
+
 output "cluster_name" {
-  value       = google_container_cluster.time_api_cluster.name
+  value       = data.google_container_cluster.time_api_cluster.name
   description = "GKE Cluster Name"
 }
 
 output "cluster_endpoint" {
-  value       = google_container_cluster.time_api_cluster.endpoint
+  value       = data.google_container_cluster.time_api_cluster.endpoint
   description = "GKE Cluster Host"
 }
 
 output "cluster_ca_certificate" {
-  value       = google_container_cluster.time_api_cluster.master_auth[0].cluster_ca_certificate
+  value       = data.google_container_cluster.time_api_cluster.master_auth[0].cluster_ca_certificate
   description = "GKE Cluster CA Certificate"
   sensitive   = true
 }
